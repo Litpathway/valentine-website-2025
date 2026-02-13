d268af81d1e5d3620fde234939a9d9f8f97a06e2
Folorunsho Daniel <117745017+Litpathway@users.noreply.github.com>
2026-02-14 00:12:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -84,13 +84,14 @@ const CONFIG = {
     // Background Music (Optional)
     // Add your own music URL after getting proper licenses
     music: {
-        enabled: true,                     // Music feature is enabled
-        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
-        startText: "🎵 Play our song",     // Button text to start music
-        stopText: "🔇 Stop music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
-    }
+          enabled: true,
+          autoplay: false,
+          fileName: "JVKE_-_her_(mp3.pm).mp3",
+          startText: "🎵 Play our song",
+          stopText: "🔇 Stop music",
+          volume: 0.8
+}
+
 };
 
 // Don't modify anything below this line unless you know what you're doing
